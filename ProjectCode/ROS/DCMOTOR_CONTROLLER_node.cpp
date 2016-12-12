d90ae4943582ff28d650a8b34a3202d072e38b66
augustusellis <augustusellis@gmail.com>
2016-12-12 10:25:22 +0000

    Beginning of ROS Nodes

--- ProjectCode/ROS/DCMOTOR_CONTROLLER_node.cpp
+++ ProjectCode/ROS/DCMOTOR_CONTROLLER_node.cpp
@@ -1,26 +1,32 @@
-#include "ROS.h"
-#include "DCMOTOR_CONTROLLER.h"
+#include <ros/ros.h>
+//#include "DCMOTOR_CONTROLLER.h"
 #include <unistd.h>
 #include "std_msgs/String.h"
+
+void eqep_Callback(const std_msgs::String::ConstPtr& msg){}
 
 int main(int argc, char **argv)
 {
   ros::init(argc,argv,"DCMOTOR_CONTROLLER_node"); // third argument is name of node
   ros::NodeHandle nh;
-  ros::Publisher pub=nh.advertise<std_msgs::String>("control_signal",1000); // control_signal is to
+  ros::Publisher control_pub=nh.advertise<std_msgs::String>("control_signal",1000); // control_signal is to
+  ros::Subscriber control_sub = n.subscribe("control_signal", 1000, eqep_Callback);
   ros::Rate loop_rate(10);
 
   int count = 0;
   while(ros::ok()){
     std_msgs::String msg; // message object, which will be stuffed with data
 
-    // put stuff that will go into msg here
+    std::stringstream ss;
+    ss << count;
+    msg.data = ss.str();
 
-    pub.publish(msg); // publishes msg to topic "control_signal"
+    control_pub.publish(msg); // publishes msg to topic "control_signal"
 
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count; // count doesnt really do anything for us in this script
+    ++count;
+    ++count;
   }
 }
--- ProjectCode/ROS/DCMOTOR_node.cpp
+++ ProjectCode/ROS/DCMOTOR_node.cpp
@@ -1,6 +1,10 @@
-#include "ROS.h"
-#include "DCMOTOR.h"
+#include <ros/ros.h>
+//#include "DCMOTOR.h"
 #include <unistd.h>
+
+void control_Callback(const std_msgs::String::ConstPtr& msg){
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
 
 int main(int argc, char **argv)
 {
@@ -8,6 +12,6 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   ros::Rate loop_rate(10);
 
-  ros::Subscriber sub = n.subscribe();
+  ros::Subscriber sub = n.subscribe("control_signal", 1000, control_Callback);
 
 }
